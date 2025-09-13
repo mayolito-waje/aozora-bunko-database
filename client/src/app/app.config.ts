9fb447e1cdddceeb306bc2b63ccb73609d1a3461
mayolito-waje <mayolito.waje08@gmail.com>
2025-09-13 21:33:01 +0800

    use app initializer

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,19 +1,28 @@
 import {
   ApplicationConfig,
+  inject,
+  provideAppInitializer,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import authInterceptor from '../core/interceptors/auth-interceptor';
+import { InitService } from '../core/services/init-service';
+import { lastValueFrom } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes),
+    provideRouter(routes, withViewTransitions()),
     provideHttpClient(withInterceptors([authInterceptor])),
+    provideAppInitializer(async () => {
+      const initService = inject(InitService);
+
+      return lastValueFrom(initService.init());
+    }),
   ],
 };
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,33 +1,13 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { Nav } from "../layouts/nav/nav";
-import { Toast } from "../layouts/toast/toast";
-import { User } from '../types/user';
-import { AccountService } from '../core/services/account-service';
-import { ToastService } from '../core/services/toast-service';
+import { Nav } from '../layouts/nav/nav';
+import { Toast } from '../layouts/toast/toast';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Nav, Toast],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App implements OnInit {
-  protected readonly title = signal('client');
-  private toastService = inject(ToastService);
-  private accountService = inject(AccountService);
-
-  async ngOnInit() {
-    this.setCurrentUser();
-  }
-
-  setCurrentUser() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-
-    const user: User = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-    this.toastService.update("ログインしました", "success");
-  }
-}
+export class App {}
